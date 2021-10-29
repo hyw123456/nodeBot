632fbca6212deb35baaedc8bb43a86646ae1650c
hyw <727295117@qq.com>
2021-10-29 20:24:25 +0800

    a

--- group.js
+++ group.js
@@ -3,6 +3,8 @@ const config = require('./config.js');
 const util = require('./util.js');
 const textGet = require('./text.js')
 const schedule = require('node-schedule');
+let isSS = true
+
 schedule.scheduleJob('2 0 8 * * *', () => {
     sendST(undefined, '今日份')
 });
@@ -42,6 +44,7 @@ async function repeat(body) {
 
 async function recall(body) {
     if ([2382843038, 727295117].includes(body.operator_id)) return
+    if(!enableGroup.includes(body.group_id)) return
     if (body.notice_type === 'group_recall') {
         const res = await needle('GET', config.url + '/get_msg', {message_id: body.message_id}, {})
         const msg = res.body.data
@@ -65,13 +68,18 @@ async function recall(body) {
                 needle('GET', config.url + '/send_group_msg', {group_id: msg.group_id, message: senMsg}, {})
             } else {
                 senMsg = await util.postMsgToSendMsg(msg.message)
-                if(Math.random() > 0.5) {
+                if(isSS && Math.random() > 0.6 ) {
+                    isSS = false
+                    setTimeout(() => {
+                        isSS = true
+                    }, 2*60*1000);
                     needle('GET', config.url + '/send_group_msg', {
                         group_id: 655389537,
                         message: message + senMsg
                     }, {})
                     senMsg = util.getRandomByList(['周末穿女装给你们爽爽','等我雌堕给你们爽','鶸酱c我','不想当♂的了',
-                        '晚上好无聊，老地址来个南桐','cpdd,我是0','感觉不如原神...牛子','戳我XP了','我就是RPQ，怎么了','昨天嫖了个伪娘，感觉真TM爽'])
+                        '晚上好无聊，老地址来个南桐','cpdd,我是0','感觉不如原神...牛子','戳我XP了','别来找我了，我不是南桐，那天只是喝多了',
+                        '我就是RPQ，怎么了','昨天嫖了个伪娘，感觉真TM爽','我是傻逼','你绿我也没关系，我喜欢你'])
                 }
                 needle('GET', config.url + '/send_group_msg', {
                     group_id: msg.group_id,
@@ -91,11 +99,16 @@ function maybeSend(body) {
         let type = params[2]
         switch (type) {
             case  '':
-                return sendMsgsST(+count, body, false);
+                 sendMsgsST(+count, body, false) 
+                 break;
+            case '社死': 
+                isSS = true
+                break;
             case  '涩':
             case  '色':
             case  '瑟':
-                return sendMsgsST(+count, body, true);
+                 sendMsgsST(+count, body, true)
+                  break;
             default: replyAtOther(+count,body ,type)
                 break
 
