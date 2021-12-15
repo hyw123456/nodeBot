86225e6fa60f8262025c2dec146d876ef2a3cd1d
Yiwang <727295117@qq.com>
2021-12-15 13:23:36 +0800

    。

---
+++ guguService.js
@@ -0,0 +1,34 @@
+//express_demo.js 文件
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+//用body parser 来解析post和url信息中的参数
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+var cors = require('cors')
+var corsOptions = {
+    origin: '*', //
+    optionsSuccessStatus: 200
+}
+
+const {translateEachOther}  = require('./gugu')
+app.post('/', cors(corsOptions), function (req, res) {
+    const body = req.body
+    const text = body.text||''
+    res.send({
+        text: text,
+        result: translateEachOther(text)
+    });
+})
+
+
+var server = app.listen(8082, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("应用实例，访问地址为 http://%s:%s", host, port)
+
+})
